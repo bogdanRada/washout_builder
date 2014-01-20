1d4d6e7ec3c587cf206ffaa326339ff6cd3d1b4f
bogdanRada <raoul_ice@yahoo.com>
2014-01-20 12:36:17 +0200

    option to specify custom urls

--- lib/washout_builder/engine.rb
+++ lib/washout_builder/engine.rb
@@ -1,9 +1,7 @@
 module WashoutBuilder
   class Engine < ::Rails::Engine
+    isolate_namespace WashoutBuilder
     initializer "wash_out.configuration" do |app|
-      app.routes.append do
-        match "/washout"   => "washout_builder#all", :via => :get, :format => false
-      end
       if app.config.wash_out[:catch_xml_errors]
         app.config.middleware.insert_after 'ActionDispatch::ShowExceptions',  WashOut::Middleware  if defined?(WashOut::Middleware)
         app.config.middleware.insert_after 'ActionDispatch::ShowExceptions',  WashOut::Middlewares::Catcher  if defined?(WashOut::Middlewares::Catcher)
