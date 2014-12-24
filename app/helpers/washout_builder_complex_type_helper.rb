423e29ef918425c1acd39dbc2743f4b8df470207
Rada Bogdan Raul <raoul_ice@yahoo.com>
2014-12-24 11:05:55 +0200

    documentation

--- app/helpers/washout_builder_complex_type_helper.rb
+++ app/helpers/washout_builder_complex_type_helper.rb
@@ -1,6 +1,8 @@
 module WashoutBuilderComplexTypeHelper
   
-  
+   # this method is for printing the attributes of a complex type
+   # if the attributes are primitives this will show the attributes with  blue color
+   # otherwise will call another method for printing the complex attribute
   def create_element_type_html(pre, element)
     element.type = "string" if element.type == "text"
     element.type = "integer" if element.type == "int"
@@ -11,7 +13,9 @@ module WashoutBuilderComplexTypeHelper
       end
   end
   
-  
+ 
+  # checks if a complex attribute of a complex type is a array or not
+  # and retrieves the complex class name of the attribute and prints it 
   def create_complex_element_type_html(pre, element)
     complex_class = element.get_complex_class_name
     unless  complex_class.nil?
--- app/helpers/washout_builder_fault_type_helper.rb
+++ app/helpers/washout_builder_fault_type_helper.rb
@@ -1,27 +1,36 @@
 module WashoutBuilderFaultTypeHelper
   
+  # checks if a complex attribute of a complex type SoapFault is array or not
+  # if the attribute is an array will print also the type of the elements contained in the array
+  # otherwise will show the complex class of the attribute
   def create_fault_model_complex_element_type(pre, attr_primitive, attribute, array)
     attribute_primitive = array == true ? "Array of #{attr_primitive}" : "#{attr_primitive}"
     pre << "<a href='##{attr_primitive}'><span class='lightBlue'> #{attribute_primitive}</span></a>&nbsp;<span class='bold'>#{attribute}</span>"
   end
   
-  
+  # if the attribute is an array  this method is used to identify the type of the elements inside the array
   def member_type_is_basic?(attr_details)
     WashoutBuilder::Type::BASIC_TYPES.include?(attr_details[:member_type].to_s.downcase) ? attr_details[:member_type].to_s.downcase : attr_details[:member_type]
   end
-  
+  # checks is the attribute has a primitive value or a complex value
   def primitive_type_is_basic?(attr_details)
-   WashoutBuilder::Type::BASIC_TYPES.include?(attr_details[:primitive].to_s.downcase) 
+    WashoutBuilder::Type::BASIC_TYPES.include?(attr_details[:primitive].to_s.downcase) 
   end
 
+  # if the attribute value is of type nil the documentation will show string 
+  # otherwise the primitive value
   def get_primitive_type_string(attr_details)
     attr_details[:primitive].to_s.downcase == "nilclass" ? "string" : attr_details[:primitive].to_s.downcase
   end  
   
+  # if the attribute is of type array the method identifies the type of the elements inside the array
   def get_member_type_string(attr_details)
     attr_details[:primitive].to_s.downcase == "array" ? member_type_is_basic?(attr_details) :  attr_details[:primitive]
   end
   
+  # this method is used to print all attributes of a SoapFault element 
+  # if the attribute value is a primitve value it will be shown in blue and will also show the type of the primitive
+  # if is a complex type will use another method for finding out the complex class 
   def create_html_fault_model_element_type(pre, attribute, attr_details)
     if primitive_type_is_basic?(attr_details) || attr_details[:primitive] == "nilclass" 
       pre << "<span class='blue'>#{get_primitive_type_string(attr_details)}</span>&nbsp;<span class='bold'>#{attribute}</span>"
