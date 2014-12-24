032eb661a2a09c5010810c62864f07e7c1f0c4a5
Rada Bogdan Raul <raoul_ice@yahoo.com>
2014-12-24 10:32:22 +0200

    documentation

--- app/controllers/washout_builder/washout_builder_controller.rb
+++ app/controllers/washout_builder/washout_builder_controller.rb
@@ -2,6 +2,10 @@ require_relative "../../../lib/washout_builder/document/generator"
 class WashoutBuilder::WashoutBuilderController < ActionController::Base
   protect_from_forgery
 
+  # Will show all api services if no name parameter is receiverd
+  # If a name parameter is present wiill try to use that and find a controller 
+  # that was that name by camelcasing the name . 
+  # IF a name is provided will show the documentation page for that controller 
   def all
     route = params[:name].present? ? controller_is_a_service?(params[:name]) : nil
     if route.present?
@@ -16,12 +20,13 @@ class WashoutBuilder::WashoutBuilderController < ActionController::Base
   
   private
   
-  
+  # tries to find all services by searching through the rails controller 
+  # and returns their namespace, endpoint and a documentation url
   def all_services
-    @map_controllers = map_controllers
+    @map_controllers = map_controllers do |route|  route.defaults[:controller]   end
     @services = @map_controllers.blank? ? [] : @map_controllers.map do |controller_name|
       {
-        'service_name' =>   controller_name.camelize  ,
+        'service_name' =>   controller_naming(controller_name)  ,
         'namespace' => service_namespace(controller_name),
         'endpoint' => service_endpoint(controller_name),
         'documentation_url' => service_documentation_url(controller_name),
@@ -32,36 +37,53 @@ class WashoutBuilder::WashoutBuilderController < ActionController::Base
       :content_type => 'text/html'
   end
   
-  def all_controllers
-    Rails.application.routes.routes
+  # the way of converting from controller string in downcase in camelcase 
+  def controller_naming(controller)
+    controller.camelize
   end
   
-  def map_controllers
-    all_controllers.map do |route|
-      route.defaults[:controller]   if route.defaults[:action] == "_generate_wsdl"
-    end.uniq.compact
+  # checking if a route has the action for generating WSDL
+  def route_can_generate_wsdl?(route)
+    route.defaults[:action] == "_generate_wsdl"
   end
   
+  # method for getting all controllers that have the generate wsdl action or finding out
+  # if a single controller is a soap service
+  def map_controllers(action = "map")
+    res =  Rails.application.routes.routes.send(action) do |route|
+      if route_can_generate_wsdl?(route)
+        yield route if block_given?
+      end
+    end
+    res = res.uniq.compact if action == "map"
+    return res
+  end
+  
+  # checking if a controller is a soap service
   def  controller_is_a_service?(controller)
-    route = all_controllers.detect do |route|
-      route.defaults[:controller].camelize == controller.camelize && route.defaults[:action] == "_generate_wsdl"
+    map_controllers("detect") do |route|
+      controller_naming(route.defaults[:controller]) == controller_naming(controller) 
     end
   end
 
+  # getting the controller class from the controller string
   def controller_class(controller)
-    "#{controller}_controller".camelize.constantize
+    controller_naming("#{controller}_controller").constantize
   end
   
+  # retrieves the service namespace
   def service_namespace(controller_name)
     controller_class(controller_name).soap_config.namespace
   end
   
+  # the endpoint is based on the namespace followed by /action suffix 
   def service_endpoint(controller_name)
     service_namespace(controller_name).gsub("/wsdl", "/action")
   end
   
+  # constructs the documentation url for a specific web service 
   def service_documentation_url(controller_name)
-    "#{washout_builder.root_url}#{controller_name.camelize}"
+    "#{washout_builder.root_url}#{controller_naming(controller_name)}"
   end
 
 end
