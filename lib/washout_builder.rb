a2d021f1414040cf385fb968c2b24faaaa13601a
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-01-07 13:03:13 +0200

    explanation

--- lib/washout_builder.rb
+++ lib/washout_builder.rb
@@ -51,13 +51,32 @@ end
 
 
 WashOut::Param.class_eval do
-   
+  
   def self.parse_builder_def(soap_config, definition)
     raise RuntimeError, "[] should not be used in your params. Use nil if you want to mark empty set." if definition == []
     return [] if definition == nil
-
-    definition = { :value => definition } unless definition.is_a?(Hash)
-
+    
+    # the following lines was removed because when generating the documentation
+    #  the "source_class" attrtibute of the object was not the name of the class of the complex tyoe
+    # but instead was the name given in the hash
+    # Example :
+    #  class ProjectType < WashOut::Type
+    #  map :project => {
+    # :name                                    => :string,
+    #  :description                           => :string,
+    #  :users                                    => [{:mail => :string }],
+    #  }
+    #end
+    # 
+    # The name of the complex type should be ProjectType and not "project"
+    
+    
+    #     if definition.is_a?(Class) && definition.ancestors.include?(WashOut::Type)
+    #        definition = definition.wash_out_param_map
+    #    end
+    
+    definition = { :value => definition } unless definition.is_a?(Hash) # for arrays and symbols
+    
     definition.collect do |name, opt|
       if opt.is_a? WashOut::Param
         opt
@@ -65,9 +84,9 @@ WashOut::Param.class_eval do
         WashOut::Param.new(soap_config, name, opt[0], true)
       else
         WashOut::Param.new(soap_config, name, opt)
+        end
       end
     end
-  end
   
 end
   
