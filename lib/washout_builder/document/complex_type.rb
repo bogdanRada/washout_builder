fd0b924296fb93b4acba525822e631f18d67815d
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-08-25 18:16:09 +0300

    documentation for complex types

--- lib/washout_builder/document/complex_type.rb
+++ lib/washout_builder/document/complex_type.rb
@@ -49,6 +49,11 @@ module WashoutBuilder
         classified? ? get_class_ancestors(config, complex_class, defined) : nil
       end
 
+      # iterates through all the elements of the current object
+      # and constructs a hash that has as keys the element names and as value their type
+
+      # @return [Hash] THe hash that contains information about the structure of the current object as complex type
+      # @api public
       def find_param_structure
         map.each_with_object({}) do|item, memo|
           memo[item.name] = item.type
@@ -56,10 +61,25 @@ module WashoutBuilder
         end
       end
 
+      # removes from this current object the elements that are inherited from other objects
+      # and set the map of the curent object to the new value
+      #
+      # @param [Array<String>] keys An array with the keys that need to be removed from current object
+      # @return [void]
+      # @api public
       def remove_type_inheritable_elements(keys)
         self.map = map.delete_if { |element| keys.include?(element.name) }
       end
 
+      # Dirty hack to fix the first washout param type.
+      # This only applies if the first complex type is inheriting WashOutType
+      # its name should be set to its descendant  and the map of the current object will be set to its descendant
+      # @see  WashOut::Param#parse_builder_def
+      #
+      # @param [WashOut::SoapConfig] config an object that holds the soap configuration
+      # @param [Class, String] complex_class the name of the complex type either as a string or a class
+      # @return [void]
+      # @api public
       def fix_descendant_wash_out_type(config, complex_class)
         param_class = begin
           complex_class.is_a?(Class) ? complex_class : complex_class.constantize
@@ -72,6 +92,14 @@ module WashoutBuilder
         self.map = descendant.map
       end
 
+      # Method that is used to check if the current object has exactly same structure as one of his ancestors
+      # if it is true, will return true, otherwise will first remove the inheritated elements from his ancestor and then return false
+      # @see #find_param_structure
+      # @see #remove_type_inheritable_elements
+      #
+      # @param [WasOut::Param] ancestor The complex type that is used to compare to the current complex type
+      # @return [Boolean] returns true if both objects have same structure, otherwise will first remove the inheritated elements from his ancestor and then return false
+      # @api public
       def same_structure_as_ancestor?(ancestor)
         param_structure = find_param_structure
         ancestor_structure = ancestor.find_param_structure
@@ -83,6 +111,14 @@ module WashoutBuilder
         end
       end
 
+      # Mehod that is used to get the ancestors of the current complex type
+      # the method will not filter the results by rejecting the classes 'ActiveRecord::Base', 'Object', 'BasicObject', 'WashOut::Type'
+      # @see WashoutBuilder::Document::SharedComplexType#get_complex_type_ancestors
+      #
+      # @param [Class, String] class_name the name of the on object that is used to fetch his ancestors
+      # @return [Array<Class>] Returns an array with all the classes from each the object inherits from but filters the results and removes the classes
+      #  'ActiveRecord::Base', 'Object', 'BasicObject', 'WashOut::Type'
+      # @api public
       def get_ancestors(class_name)
         param_class = begin
           class_name.is_a?(Class) ? class_name : class_name.constantize
@@ -96,6 +132,14 @@ module WashoutBuilder
         end
       end
 
+      # Method used to fetch the descendants of the current object
+      # @see #get_nested_complex_types
+      # @see WashOutParam#struct?
+      #
+      # @param [WashOut::SoapConfig] config an object that holds the soap configuration
+      # @param [Array<Hash>] defined An Array with all the complex types that have been detected till now
+      # @return [Array<Hash>] An array with all the complex types that
+      # @api public
       def complex_type_descendants(config, defined)
         if struct?
           c_names = []
@@ -105,6 +149,17 @@ module WashoutBuilder
         defined
       end
 
+      # Recursive method that tries to identify all the nested descendants of the current object
+      # @see #find_complex_class_name
+      # @see #fix_descendant_wash_out_type
+      # @see #complex_type_hash
+      # @see #complex_type_ancestors
+      # @see #complex_type_descendants
+      #
+      # @param [WashOut::SoapConfig] config holds the soap configuration
+      # @param [Array<Hash>] defined An array with all the complex type structures that have been detected so far
+      # @return [Array<Hash>] An array with all the complex type that have been detected while iterating to all the descendants of the current object and also contains the previous ones
+      # @api public
       def get_nested_complex_types(config, defined)
         defined = [] if defined.blank?
         complex_class = find_complex_class_name(defined)
@@ -116,10 +171,23 @@ module WashoutBuilder
         defined.sort_by { |hash| hash[:class].to_s.downcase }.uniq unless defined.blank?
       end
 
+      # method that constructs the a hash with the name of the ancestor ( the class name) and as value its elemen structure
+      # @see WashOut::Type#wash_out_param_map
+      #
+      # @param [WashoutType] ancestors The class that inherits from WashoutType
+      # @return [Hash] A hash that has as a key the class name in downcase letters and as value the mapping of the class attributes
+      # @api public
       def ancestor_structure(ancestors)
         { ancestors[0].to_s.downcase => ancestors[0].wash_out_param_map }
       end
 
+      # Constructs the complex type information wuth its name, with the object itself and his ancestors
+      #
+      # @param [Class] class_name The name of the class
+      # @param [WashOut::Param] object The object itself
+      # @param [Array<Class>] ancestors An array with all the ancestors that the object inherits from
+      # @return [Hash] A hash with that contains the params sent to the method
+      # @api public
       def complex_type_hash(class_name, object, ancestors)
         {
           class: class_name,
@@ -128,6 +196,17 @@ module WashoutBuilder
         }
       end
 
+      # A recursive method that fetches the ancestors of a given class (that inherits from WashoutType)
+      # @see #get_ancestors
+      # @see  WashOut::Param#parse_def
+      # @see #same_structure_as_ancestor?
+      # @see #complex_type_hash
+      #
+      # @param [WashOut::SoapConfig] config holds the soap configuration
+      # @param [Class] class_name  The name of the class that is used for fetching the ancestors
+      # @param [Array<Hash>] defined An Array with all the complex types that have been detected so far
+      # @return [Array<Class>] An Array of classes from which the class that is sent as parameter inherits from
+      # @api public
       def get_class_ancestors(config, class_name, defined)
         ancestors = get_ancestors(class_name)
         return if ancestors.blank?
