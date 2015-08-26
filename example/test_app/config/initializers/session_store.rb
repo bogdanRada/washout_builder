83f4ab0d412e24089bf8cdcb57be9c6f3d46eb26
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-08-26 14:05:23 +0300

    remove example app

--- example/test_app/config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store,
-                                              :key => '_app_tracker_session',
-                                              :secure => Rails.env.production?,
-                                              :httponly => true
