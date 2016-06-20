ed46e2e2f398d268142bc51056138ea8f2ad7b76
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-06-21 00:23:20 +0300

    automatically mounted engine

--- lib/washout_builder/engine.rb
+++ lib/washout_builder/engine.rb
@@ -2,7 +2,13 @@ module WashoutBuilder
   # the engine that is used to mount inside the rails application
   class Engine < ::Rails::Engine
     isolate_namespace WashoutBuilder
-    initializer 'washout_builder.configuration' do |_app|
+    config.washout_builder = ActiveSupport::OrderedOptions.new
+    initializer 'washout_builder.configuration' do |app|
+      if app.config.washout_builder[:mounted_path]
+        app.routes.append do
+          mount WashoutBuilder::Engine => app.config.washout_builder[:mounted_path]
+        end
+      end
     end
   end
 end
