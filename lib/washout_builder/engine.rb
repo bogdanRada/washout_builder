cd0976c7b4458f385b0a67d48df030f72d4afff9
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-06-21 10:55:06 +0300

    added options to blacklist or whitelist the environment where washout_builder can be mounted

--- lib/washout_builder/engine.rb
+++ lib/washout_builder/engine.rb
@@ -4,9 +4,16 @@ module WashoutBuilder
     isolate_namespace WashoutBuilder
     config.washout_builder = ActiveSupport::OrderedOptions.new
     initializer 'washout_builder.configuration' do |app|
-      if app.config.washout_builder[:mounted_path]
-        app.routes.append do
-          mount WashoutBuilder::Engine => app.config.washout_builder[:mounted_path]
+      blacklisted_envs = app.config.washout_builder[:blacklisted_envs]
+      blacklisted_envs = blacklisted_envs.is_a?(Array) ? blacklisted_envs : [blacklisted_envs].compact
+      whitelisted_envs = app.config.washout_builder[:whitelisted_envs]
+      whitelisted_envs = whitelisted_envs.is_a?(Array) ? whitelisted_envs : [whitelisted_envs].compact
+      mounted_path = app.config.washout_builder[:mounted_path]
+      if (whitelisted_envs.present? || blacklisted_envs.present?) && whitelisted_envs.find{|a| blacklisted_envs.include?(a) }.blank?
+        if whitelisted_envs.include?('*') || (!blacklisted_envs.include?(Rails.env) || whitelisted_envs.include?(Rails.env))
+          app.routes.append do
+            mount WashoutBuilder::Engine => mounted_path if mounted_path.is_a?(String) && mounted_path.starts_with?('/')
+          end
         end
       end
     end
